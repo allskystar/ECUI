4e35446702903a4a59917027e0d4795723562b37
ouyangxianwei <ouyangxianwei@yxqiche.com>
2018-07-17 20:34:59 +0800

    添加注释

--- src/base/count-down.js
+++ src/base/count-down.js
@@ -1,6 +1,13 @@
 /*
 @example
-<div ui="type:count-down"></div>
+<div ui="type:count-down;time:200"></div>
+
+@fields
+_bMillisecond   - 是否精确到毫秒，默认精确到秒
+_bImmediate     - 是否直接开始倒计时
+_nTime          - 倒计时的时长，单位秒
+_nDown          - 倒计时剩余的时长，单位秒
+_oTimer         - 定时器
 */
 (function () {
 //{if 0}//
@@ -8,6 +15,12 @@
         ui = core.ui,
         util = core.util;
 //{/if}//
+    /**
+     * 格式化时间。
+     * @private
+     *
+     * @param {number} time 剩余的时间，单位秒
+     */
     function formatTime(time) {
         var hours = Math.floor(time / 3600),
             minutes = Math.floor((time % 3600) / 60),
@@ -18,6 +31,14 @@
         return (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (second < 10 ? '0' + second : second);
     }
 
+    /**
+     * 计时器控件。
+     * options 属性：
+     * time        倒计时的时长
+     * millisecond 倒计时是否以毫秒为单位，默认为false
+     * immediate   是否直接开始倒计时，默认为false
+     * @control
+     */
     ui.CountDown = core.inherits(
         ui.Control,
         function (el, options) {
@@ -28,6 +49,9 @@
             this.setContent(formatTime(this._nTime));
         },
         {
+            /**
+             * @override
+             */
             $ready: function (event) {
                 ui.Control.prototype.$ready.call(this, event);
                 if (this._bImmediate) {
@@ -35,15 +59,31 @@
                 }
             },
 
-            clear: function () {
+            /**
+             * 复位定时器。
+             * @public
+             */
+            reset: function () {
                 this.stop();
                 this.setContent(formatTime(this._nTime));
             },
 
+            /**
+             * 设置定时器时间。
+             * @public
+             *
+             * @param {number} time 定时器时间
+             */
             setTime: function (time) {
                 this._nTime = time;
             },
 
+            /**
+             * 定时器开始。
+             * @public
+             *
+             * @param {boolean} isContinue 是否从终止状态继续，否则重新计时
+             */
             start: function (isContinue) {
                 if (this._oTimer) {
                     this._oTimer();
@@ -63,6 +103,10 @@
                 }, -100, this);
             },
 
+            /**
+             * 定时器终止。
+             * @public
+             */
             stop: function () {
                 this._oTimer();
             }
