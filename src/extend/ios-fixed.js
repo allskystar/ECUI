68bc44e514a5982ba5f8f53f398e6c1e2eb58eb8
ouyangxianwei <ouyangxianwei@yxqiche.com>
2018-08-02 11:00:33 +0800

    ios软键盘事件处理

--- src/extend/ios-fixed.js
+++ src/extend/ios-fixed.js
@@ -31,10 +31,6 @@ ios-fixed - ios下的fixed定位插件，修复软键盘不能正确定位的问
             dispose: function () {
                 util.remove(topList, this);
                 util.remove(bottomList, this);
-            },
-
-            mousemove: function (event) {
-                event.preventDefault();
             }
         },
 
