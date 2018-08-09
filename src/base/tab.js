41f085969f6a8ac4d8e4c9bb1f0e20528d7229b5
allskystar <allskystar@gmail.com>
2018-08-10 06:56:59 +0800

    修复tab的item不定自定义primary class的问题

--- src/base/tab.js
+++ src/base/tab.js
@@ -106,6 +106,7 @@ _eContainer      - 容器 DOM 元素
                     if (el.tagName !== 'STRONG') {
                         var containerEl = el;
                         el = dom.first(el);
+                        options.primary = el.className.trim().split(' ')[0] || options.parent.Item.TYPES[0];
                     }
 
                     ui.Item.call(this, el, options);
