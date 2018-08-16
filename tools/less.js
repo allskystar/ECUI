8633cf1d2c267d40ffe92fd451d0bcc3c75a1121
allskystar <allskystar@gmail.com>
2018-08-17 00:27:39 +0800

    控制link的加载顺序

--- tools/less.js
+++ tools/less.js
@@ -724,16 +724,20 @@ function loadStyleSheet(sheet, callback, reload, remaining, modifyVars) {
 }
 
 function loadStyleSheets(callback, reload, modifyVars) {
-    for (var i = less.sheets.length; i--; ) {
-        ecui.pause();
-    }
-    for (var i = 0; i < less.sheets.length; i++) {
-        loadStyleSheet(less.sheets[i], function () {
-            callback.apply(this, arguments);
+    function loadOne(i) {
+        if (i < less.sheets.length) {
+            ecui.pause();
+            loadStyleSheet(less.sheets[i], function () {
+                callback.apply(this, arguments);
+                ecui.resume();
+                loadOne(i + 1);
+            }, reload, less.sheets.length - (i + 1), modifyVars);
+        } else {
             ecui.resume();
-        }, reload, less.sheets.length - (i + 1), modifyVars);
+        }
     }
-    ecui.resume();
+
+    loadOne(0);
 }
 
 function initRunningMode(){
@@ -808,7 +812,7 @@ less.refresh = function (reload, modifyVars, clearFileCache) {
         } else {
             less.logger.info("rendered " + sheet.href + " successfully.");
         }
-        ecui.dom.createStyleSheet(lessFuncs(sheet, css));
+        ecui.dom.createStyleSheet(lessFuncs(sheet, css)).setAttribute('link', sheet.href);
         less.logger.info("css for " + sheet.href + " generated in " + (new Date() - endTime) + 'ms');
         if (webInfo.remaining === 0) {
             totalMilliseconds = new Date() - startTime;
