cbcbea62e02b19d0e8952acb2d40f9207744d8b3
OuyangXianwei <allskystar@gmail.com>
2018-11-22 21:38:37 +0800

    svg路径函数升级

--- src/svg/svg.js
+++ src/svg/svg.js
@@ -23,19 +23,35 @@
             },
 
             drawEllipse: function (x, y, rx, ry, className) {
-                this._aDrawer.push('<ellipse cx="' + x + '" cy="' + y + '" rx="' + rx + '" ry="' + ry + '" class="' + className + '"/>');
+                this._aDrawer.push('<ellipse class="' + className + '" cx="' + x + '" cy="' + y + '" rx="' + rx + '" ry="' + ry + '"/>');
             },
 
             drawLine: function (x1, y1, x2, y2, className) {
-                this._aDrawer.push('<line x1="' + x1 + '" y1="' + y1 + '" x2="' + x2 + '" y2="' + y2 + '" class="' + className + '"/>');
+                this._aDrawer.push('<line class="' + className + '" x1="' + x1 + '" y1="' + y1 + '" x2="' + x2 + '" y2="' + y2 + '"/>');
+            },
+
+            drawPathArc: function (rx, ry, half, clockwise, toX, toY) {
+                this._aDrawer.push('A ' + rx + ' ' + ry + ' 0 ' + (half ? '1 ' : '0 ') + (clockwise ? '1 ' : '0 ') + toX + ' ' + toY + ' ');
+            },
+
+            drawPathEnd: function () {
+                this._aDrawer.push('"/>');
+            },
+
+            drawPathMove: function (x, y) {
+                this._aDrawer.push('M ' + x + ' ' + y + ' ');
+            },
+
+            drawPathStart: function (className) {
+                this._aDrawer.push('<path class="' + className + '" d="');
             },
 
             drawRect: function (x, y, width, height, className) {
-                this._aDrawer.push('<rect x="' + x + '" y="' + y + '" width="' + width + '" height="' + height + '" class="' + className + '"/>');
+                this._aDrawer.push('<rect class="' + className + '" x="' + x + '" y="' + y + '" width="' + width + '" height="' + height + '"/>');
             },
 
             drawText: function (x, y, text, className) {
-                this._aDrawer.push('<text x="' + x + '" y="' + y + '" class="' + className + '">' + text + '</text>');
+                this._aDrawer.push('<text class="' + className + '" x="' + x + '" y="' + y + '">' + text + '</text>');
             },
 
             render: function () {
