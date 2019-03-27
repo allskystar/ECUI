dccd19223eb4304917a230404ce1dc9d07fc5f3c
yiche <allskystar@gmail.com>
2019-03-27 20:21:06 +0800

    增加注释

--- src/input/abstract-select.js
+++ src/input/abstract-select.js
@@ -1,11 +1,11 @@
 /*
 @example
-<select ui="type:select" name="sex">
+<select ui="type:select;placeholder:请选择" name="sex">
     <option value="male" selected="selected">男</option>
     <option value="female">女</option>
 </select>
 或
-<div ui="type:select;name:sex;value:male">
+<div ui="type:select;name:sex;value:male;placeholder:请选择">
     <div ui="value:male">男</div>
     <div ui="value:female">女</div>
 </div>
@@ -29,6 +29,7 @@ _uOptions     - 下拉选择框
      * 扩展了原生 SelectElement 的功能，允许指定下拉选项框的最大选项数量，在屏幕显示不下的时候，会自动显示在下拉框的上方。在没有选项时，下拉选项框有一个选项的高度。下拉框控件允许使用键盘与滚轮操作，在下拉选项框打开时，可以通过回车键或鼠标点击选择，上下键选择选项的当前条目，在关闭下拉选项框后，只要拥有焦点，就可以通过滚轮上下选择选项。
      * options 属性：
      * optionSize     下拉框最大允许显示的选项数量，默认为5
+     * placeholder    未选择时的提示信息
      * required       是否必须选择
      * @control
      */
