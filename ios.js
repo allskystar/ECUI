f777118d57484dfd0979f09bbb7caeccfda4bcbb
yiche <allskystar@gmail.com>
2019-05-09 15:58:39 +0800

    优化ios 11.1/11.2的滚动条操作

--- ios.js
+++ ios.js
@@ -9,6 +9,14 @@
         oldAjax(url, options);
     };
 
+    window.dispatchCustomEvent = function (eventName, options) {
+        options = JSON.parse(options);
+        var event = document.createEvent('HTMLEvents');
+        event.initEvent(eventName, true, true);
+        ecui.util.extend(event, options);
+        window.dispatchEvent(event);
+    };
+
     ecui.ready(function () {
         if (ecui.$('ECUI-FIXED-INPUT')) {
             ecui.dom.remove(ecui.$('ECUI-FIXED-INPUT'));
