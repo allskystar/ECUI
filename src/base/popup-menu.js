aa8ef02e1802f5052b0a3e3556d05211c245d23a
yiche <allskystar@gmail.com>
2019-05-22 09:16:42 +0800

    增加智能对齐方式

--- src/base/popup-menu.js
+++ src/base/popup-menu.js
@@ -1,12 +1,17 @@
 /*
 @example
-<ul ui="type:menu">
+<ul ui="type:popup-menu">
     <li>选项一</li>
     <li>选项二</li>
     <ul>
         <li>子选项组名</li>
         <li>子选项一</li>
         <li>子选项二</li>
+        <ul>
+            <li>孙选项组名</li>
+            <li>孙选项一</li>
+            <li>孙选项二</li>
+        </ul>
     </ul>
 </ul>
 */
@@ -24,7 +29,15 @@
     ui.PopupMenu = core.inherits(
         ui.Control,
         'ui-popup-menu',
+        function (el, options) {
+            ui.Control.call(this, el, options);
+            this.left = true;
+        },
         {
+            /**
+             * 菜单项部件。
+             * @unit
+             */
             Item: core.inherits(
                 ui.Item,
                 'ui-popup-menu-item',
@@ -39,10 +52,13 @@
                     ui.Item.call(this, el, options);
 
                     if (popup) {
-                        this.setPopup(core.$fastCreate(ui.PopupMenu, popup, this));
+                        this.setChildMenu(core.$fastCreate(ui.PopupMenu, popup, this));
                     }
                 },
                 {
+                    /**
+                     * @override
+                     */
                     $mouseout: function (event) {
                         ui.Item.prototype.$mouseout.call(this, event);
                         if (this._cPopup) {
@@ -50,6 +66,9 @@
                         }
                     },
 
+                    /**
+                     * @override
+                     */
                     $mouseover: function (event) {
                         ui.Item.prototype.$mouseover.call(this, event);
                         if (this._cPopup) {
@@ -58,7 +77,13 @@
                         }
                     },
 
-                    setPopup: function (popup) {
+                    /**
+                     * 设置子菜单。
+                     * @public
+                     *
+                     * @param {ecui.ui.PopupMenu} popup 弹出菜单控件
+                     */
+                    setChildMenu: function (popup) {
                         if (this._cPopup !== popup) {
                             if (this._cPopup) {
                                 this._cPopup.hide();
@@ -76,14 +101,40 @@
                 }
             ),
 
+            /**
+             * @override
+             */
             $alterItems: util.blank,
 
+            /**
+             * 菜单自动对齐。
+             * @public
+             *
+             * @param {ecui.ui.Control} control 需要对齐的控件
+             */
             assignTo: function (control) {
                 var pos = dom.getPosition(control.getMain()),
+                    controlWidth = control.getWidth(),
+                    width = this.getWidth(),
                     height  = this.getHeight(),
-                    view = util.getView();
+                    view = util.getView(),
+                    x;
 
-                this.setPosition(Math.min(pos.left + control.getWidth() - 4, view.right - this.getWidth()), Math.max(0, Math.min(pos.top - Math.round((height - control.getHeight()) / 2), view.bottom - height)));
+                if (this.left) {
+                    x = pos.left + controlWidth - 4;
+                    if (x > view.right - width) {
+                        this.left = false;
+                        x = pos.left - width + 4;
+                    }
+                } else {
+                    x = pos.left - width + 4;
+                    if (x < view.left) {
+                        this.left = true;
+                        x = pos.left + controlWidth - 4;
+                    }
+                }
+
+                this.setPosition(Math.max(view.left, Math.min(x, view.right - width)), Math.max(view.top, Math.min(pos.top - Math.round((height - control.getHeight()) / 2), view.bottom - height)));
             }
         },
         ui.Items
