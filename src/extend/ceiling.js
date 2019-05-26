cd234a32e4f50005a16a03d44ae420cd1bd60940
yiche <allskystar@gmail.com>
2019-05-27 07:52:26 +0800

    修复pause状态滚动时的bug

--- src/extend/ceiling.js
+++ src/extend/ceiling.js
@@ -39,12 +39,14 @@ ceiling - 吸顶插件，使用ext-ceiling的方式引用，指定的吸顶时�
             },
 
             scroll: function () {
-                var data = configures[this.getUID()],
+                var configure = configures[this.getUID()],
                     layout = dom.parent(this.getMain());
-                if (isToucher) {
-                    dom.setStyle(layout, 'transform', 'translate3D(0px,' + (Math.max(data.top, dom.getPosition(data.holder).top) + util.getView().top - util.toNumber(layout.style.top)) + 'px,0px)');
-                } else {
-                    layout.style.top = Math.max(data.top + util.getView().top, dom.getPosition(data.holder).top) + 'px';
+                if (configure.holder) {
+                    if (isToucher) {
+                        dom.setStyle(layout, 'transform', 'translate3D(0px,' + (Math.max(configure.top, dom.getPosition(configure.holder).top) + util.getView().top - util.toNumber(layout.style.top)) + 'px,0px)');
+                    } else {
+                        layout.style.top = Math.max(configure.top + util.getView().top, dom.getPosition(configure.holder).top) + 'px';
+                    }
                 }
             }
         },
@@ -53,49 +55,55 @@ ceiling - 吸顶插件，使用ext-ceiling的方式引用，指定的吸顶时�
             var el = control.getMain(),
                 configure = configures[control.getUID()];
 
-            configure.cssText = el.style.cssText;
-            el.style.position = 'relative';
-            el.style.top = (dom.getPosition(dom.parent(el)).top - dom.getPosition(configure.holder).top) + 'px';
+            if (configure.holder) {
+                configure.cssText = el.style.cssText;
+                el.style.position = 'relative';
+                el.style.top = '0px';
+                el.style.top = (dom.getPosition(dom.parent(el)).top - dom.getPosition(configure.holder).top) + 'px';
 
-            document.body.removeChild(dom.parent(el));
-            dom.insertBefore(el, configure.holder);
-            dom.remove(configure.holder);
+                document.body.removeChild(dom.parent(el));
+                dom.insertBefore(el, configure.holder);
+                dom.remove(configure.holder);
+                delete configure.holder;
+            }
         },
 
         resume: function (control) {
             var el = control.getMain(),
                 configure = configures[control.getUID()];
 
-            if (configure.hasOwnProperty('cssText')) {
-                el.style.cssText = configure.cssText;
-                delete configure.cssText;
-            }
+            if (!configure.holder) {
+                if (configure.hasOwnProperty('cssText')) {
+                    el.style.cssText = configure.cssText;
+                    delete configure.cssText;
+                }
 
-            var width = control.getWidth(),
-                pos = dom.getPosition(el),
-                layout = dom.create(
-                    {
-                        style: {
-                            position: 'absolute',
-                            top: pos.top + 'px',
-                            width: width + 'px'
+                var width = control.getWidth(),
+                    pos = dom.getPosition(el),
+                    layout = dom.create(
+                        {
+                            style: {
+                                position: 'absolute',
+                                top: Math.max(pos.top, configure.top) + 'px',
+                                width: width + 'px'
+                            }
                         }
-                    }
+                    );
+
+                configure.holder = dom.insertBefore(
+                    dom.create(
+                        {
+                            style: {
+                                width: width + 'px',
+                                height: control.getHeight() + 'px'
+                            }
+                        }
+                    ),
+                    el
                 );
-
-            configure.holder = dom.insertBefore(
-                dom.create(
-                    {
-                        style: {
-                            width: width + 'px',
-                            height: control.getHeight() + 'px'
-                        }
-                    }
-                ),
-                el
-            );
-            layout.appendChild(el);
-            document.body.appendChild(layout);
+                layout.appendChild(el);
+                document.body.appendChild(layout);
+            }
         }
     };
 }());
